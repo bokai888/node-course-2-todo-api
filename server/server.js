8dcfef7c9002c78b77517bc6fc8fbd863010d452
bokai888 <ktabor25@gmail.com>
2019-02-06 14:10:36 -0600

    Setup separate test and developemnt envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 let express = require('express');
 let bodyParser = require('body-parser');
@@ -9,7 +11,7 @@ let {User} = require('./modles/user');
 
 let app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
